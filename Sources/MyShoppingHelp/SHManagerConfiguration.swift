8c73c4f6d83aae9254fa672be9f89fab3c33f711
Mihai Fratu <zeusent@msn.com>
2025-09-08 12:02:42 +0300

    Made redirect path an optional config param

--- Sources/MyShoppingHelp/SHManagerConfiguration.swift
+++ Sources/MyShoppingHelp/SHManagerConfiguration.swift
@@ -12,11 +12,11 @@ public struct SHManagerConfiguration {
     public let redirectUrlProtocol: String
     public let redirectUrlPath: String
     
-    public init(clientId: String, appGroupId: String? = nil, redirectUrlProtocol: String, redirectUrlPath: String) {
+    public init(clientId: String, appGroupId: String? = nil, redirectUrlProtocol: String, redirectUrlPath: String?) {
         self.clientId = clientId
         self.appGroupId = appGroupId
         self.redirectUrlProtocol = redirectUrlProtocol
-        self.redirectUrlPath = redirectUrlPath
+        self.redirectUrlPath = redirectUrlPath ?? "msh/callback"
     }
     
 }
